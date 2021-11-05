c3033226972d6723a1009f4e7799f63e5b3c7c72
ishay320 <ishay320@hotmail.com>
2021-11-05 10:37:59 +0200

    reformat and cleanup

--- sorting/sketch.js
+++ sorting/sketch.js
@@ -1,29 +1,30 @@
 
-const hight = window.innerHeight;
-const width = window.innerWidth;
+const window_hight = window.innerHeight;
+const window_width = window.innerWidth;
 
-var random_array = []
+var main_array = []
 const array_size = 100
 
 function setup() {
-  createCanvas(width, hight);
+  createCanvas(window_width, window_hight);
   for (let index = 0; index < array_size; index++) {
-    random_array.push(int(random(1, 100)));
+    main_array.push(int(random(1, 100)));
   }
 }
 
-const wide = width / array_size
-let i = 0
-let j = 0
-var speed = 4
+
+var i = 0
+var j = 0
+const speed = 4
 
 function draw() { //TODO: understand how to make it slow 
   background(220);
+  // doing `speed` element at one draw
   for (let index = 0; index < speed; index++) {
-    if (random_array[j] > random_array[j + 1]) {
-      var temp = random_array[j]
-      random_array[j] = random_array[j + 1]
-      random_array[j + 1] = temp
+    if (main_array[j] > main_array[j + 1]) {
+      var temp = main_array[j]
+      main_array[j] = main_array[j + 1]
+      main_array[j + 1] = temp
     }
     j++
     if (j == (array_size - i)) {
@@ -35,22 +36,23 @@ function draw() { //TODO: understand how to make it slow
     }
   }
 
-  show(random_array)
-  text("Bubble sort", (width / 2) - 100, 20, 300, 200)
+  show(main_array)
+  text("Bubble sort", (window_width / 2) - 100, 20, 300, 200)
   textSize(30)
-
 }
+
+const rect_width = window_width / array_size
 
 function show(array) {
   background(220);
-  var i = 0
-  random_array.forEach(element => {
-    rect((i++) * wide, hight, wide, -(element * 4))
+  let i = 0
+  main_array.forEach(element => {
+    rect((i++) * rect_width, window_hight, rect_width, -(element * 4))
   });
 }
 
 function swap(a, b) {
-  var temp = a
+  let temp = a
   a = b
   a = temp
 }
