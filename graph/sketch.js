f6ec91358ae83bf5a4aeaa15ec69cbd432d16d9a
ישי טרטנר <ishay320640@gmail.com>
2021-11-14 15:27:42 +0200

    started working on graphs, will be: - nodes - graph with nodes

--- graph/sketch.js
+++ graph/sketch.js
@@ -5,3 +5,37 @@ function setup() {
 function draw() {
   background(220);
 }
+
+class Node {
+  key
+  value
+  neighborArr = []
+  constructor(key, value) {
+    this.key = key
+    this.value = value
+  }
+  getValue() {
+    return this.value
+  }
+  addNeighbor(node) {
+    this.neighborArr.push(node)
+  }
+  removeNeighbor(node = Node) {
+    let pos = this.neighborArr.indexOf(node)
+    if (pos == -1) {
+      throw ("ERROR: cant find node" + node.getValue())
+    }
+    this.neighborArr.r////////////////////////
+  }
+  haveNeighbor(node) {
+    let pos = this.neighborArr.indexOf(node)
+    if (pos == -1) {
+      return false
+    }
+    return true
+  }
+}
+
+class graph {
+  nodes
+}
